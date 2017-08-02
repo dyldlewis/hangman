04a09729419d8b1ae7f01f270a81e7bff9f98f9b
Dylan Lewis, jacob ruleaux and Nicole Freed <nicole@>
2017-08-02 16:24:23 -0700

    modify getDino() method to select and display random dino

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -3,12 +3,15 @@ var Dino = require('./../js/dino.js').dinoModule;
 var displayDinos = function(dinos_array) {
   $('#output').append(dinos_array + "<br>");
 };
+var displayRandom = function(random_item) {
+  $("#random").text(random_item);
+}
 
 $(document).ready(function() {
   $('#get-dino').click(function() {
     var paragraphs = parseInt($('#paragraphs').val());
     var words = parseInt($('#words').val());
     var newDino = new Dino(paragraphs, words);
-    newDino.getDino(displayDinos);
+    newDino.getDino(displayDinos, displayRandom);
   });
 });
--- js/dino.js
+++ js/dino.js
@@ -3,16 +3,30 @@ function Dino(paragraphs, words) {
   this.words = words;
 }
 
-Dino.prototype.getDino = function(displayDinos) {
+function getRandomInt(paragraphs, words) {
+  min = 1;
+  max = paragraphs * words;
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+Dino.prototype.getDino = function(displayDinos, displayRandom) {
+  var allDinos = [];
   var paragraphs = this.paragraphs;
   var words = this.words;
+  var random_int = getRandomInt(paragraphs, words);
   $.get('http://dinoipsum.herokuapp.com/api/?format=json' +  '&paragraphs=' + paragraphs + '&words=' + words)
   .then(function(response) {
     var dinos = response;
     for (i = 0; i < paragraphs; i++) {
       var new_array = response[i].join(" ");
       displayDinos(new_array);
+      for (k = 0; k < words; k++) {
+        allDinos.push(response[i][k]);
+      }
     }
+    console.log(random_int);
+    console.log(allDinos);
+    displayRandom(allDinos[random_int]);
   })
   .fail(function(error) {
     $("#output").text(error.responseJSON.message);
