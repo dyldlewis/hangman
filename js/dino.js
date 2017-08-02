a052917ab219d2e0647e988dd598393d6dfd7f54
Dylan Lewis, Nicole Freed and jacob ruleaux <dylan+nicole+jacob@>
2017-08-02 10:01:41 -0700

    add promise and error handling to api call

--- js/dino.js
+++ js/dino.js
@@ -4,12 +4,16 @@ function Dino(paragraphs, words) {
 }
 
 Dino.prototype.getDino = function() {
-  $.get('http://dinoipsum.herokuapp.com/api/?format=json' +  '&paragraphs=' + this.paragraphs + '&words=' + this.words, function(response) {
+  $.get('http://dinoipsum.herokuapp.com/api/?format=json' +  '&paragraphs=' + this.paragraphs + '&words=' + this.words)
+  .then(function(response) {
     var dinos = response;
     for (i = 0; i < this.paragraphs; i++) {
       var new_array = response[i].join(" ");
       $('#output').append(new_array + "<br>");
     }
-}
-
+  })
+  .fail(function(error) {
+    $("#output").text(error.responseJSON.message)
+  })
+};
 exports.dinoModule = Dino;
